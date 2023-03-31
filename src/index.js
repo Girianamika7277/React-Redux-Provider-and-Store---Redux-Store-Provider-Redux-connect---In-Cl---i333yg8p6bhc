bf1f5cc7f8b313a20c27fba98c761dc98ce7918b
Anamika <116385962+Girianamika7277@users.noreply.github.com>
2023-03-31 23:18:31 +0530

    edit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 //code here 
+import store from './redux/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+
+<Provider store={store}>
+
+<App />
+
+</Provider>
+
+);
 
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
